630948e0ae9b75d5af0aea27debafc9593c3cdf0
Ruslasadov <58813459+Ruslasadov@users.noreply.github.com>
2020-02-25 18:20:52 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var name;
+name = ("Ruslan");
+alert(name); 
+var name2;
+name2 = ("People");
+document.write("Hello my name is Ruslan!");
+document.write("<div>Hello where do you live???</div>"); 
+document.write("<div>in Baku</div>");
